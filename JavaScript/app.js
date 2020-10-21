9ca6c3a4a68ea253fd47a6843b2926ac908b5e70
adarsh-mamgain <52105807+adarsh-mamgain@users.noreply.github.com>
2020-10-21 22:47:48 +0530

    Updated Index Countdown

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("July 31, 2020 00:00:00").getTime();
+var countDownDate = new Date("January 1, 2021 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
